1bcb229bbcf233f9e525761f54fe7f9bad6a6a1f
Alexander Harding <alexander.harding@software.dell.com>
2015-07-31 10:34:31 -0500

    modifying to work on docker

--- Gruntfile.js
+++ Gruntfile.js
@@ -457,7 +457,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('serve', function(target) {
         if (target === 'dist') {
-            return grunt.task.run(['build', 'express:prod', 'open', 'express-keepalive']);
+            return grunt.task.run(['build', 'express:prod', 'express-keepalive']);
         }
 
         if (target === 'debug') {
@@ -474,7 +474,6 @@ module.exports = function(grunt) {
             'concurrent:server',
             'autoprefixer',
             'express:dev',
-            'open',
             'watch'
         ]);
     });
